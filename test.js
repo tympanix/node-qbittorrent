00128f841be16dce4457184c98afaed5b4df433c
tympanix <tympanix@gmail.com>
2018-12-29 17:13:43 +0100

    add: .env file

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
-var request = require('request');
-require('request-debug')(request);
+var request = require('request')
+require('request-debug')(request)
+require('dotenv').config()
 
 const QBittorrent = require('./index.js')
 
-var q = new QBittorrent({})
+var q = new QBittorrent({
+    user: process.env.QBIT_USER,
+    pass: process.env.QBIT_PASS,
+})
 
 q.login((err) => {
     q.syncMaindata(() => {
